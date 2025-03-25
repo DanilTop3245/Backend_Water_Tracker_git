f384e646f8523e965357cb5e8eb0bd9d8f8485b8
DanilTop3245 <turasov2009@gmail.com>
2025-03-25 12:21:24 +0200

    Fixed more bags

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
+import authRouter from "./routes/authRoutes.js";
+import watersRouter from "./routes/watersRoutes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json" assert { type: "json" };
-import authRouter from "./routes/authRouter.js";
-import usersRouter from "./routes/usersRouter.js";
-import waterRateRouter from "./routes/waterRateRouter.js";
+import waterRateRouter from "./routes/waterRateRoutes.js";
+import usersRouter from "./routes/usersRoutes.js";
+import googleAuthRouter from "./routes/googleAuthRoutes.js";
 
 const app = express();
+const { DB_HOST, PORT } = process.env;
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use("/avatars", express.static("public/avatars"));
 
+app.use("/api/auth", googleAuthRouter);
+
+app.use("/api/users", authRouter, usersRouter);
 app.use("/auth", authRouter);
-app.use("/users", usersRouter);
-app.use("/water-rate", waterRateRouter);
+app.use("/api/waters", watersRouter);
+app.use("/api/water-rate", waterRateRouter);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -27,8 +49,4 @@ app.use((_, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
-
-app.listen(8080, () => {
-  console.log("Server is running. Use our API on port: 3000");
 });
--- middleware/handleMongooseError.js
+++
@@ -1,6 +0,0 @@
-export const handleMongooseError = (error, data, next) => {
-  const { name, code } = error;
-  const codeStatus = name === "MongoServerError" && code === 11000 ? 409 : 400;
-  error.status = codeStatus;
-  next();
-};
